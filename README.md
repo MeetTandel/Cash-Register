2d029e537809b747e50e777f2bcd516d159435b9
MeetTandel <meettandel47@gmail.com>
2021-09-04 16:26:30 +0530

    all files

---
+++ README.md
@@ -0,0 +1,23 @@
+# mark 10 - Cash Register
+
+Cash Register is an app that will tell you the change to be returned as well as the number of notes to be returned. It is made with vanilla JS.
+
+#### Live Project [https://87t47.csb.app/](https://87t47.csb.app/)
+
+## How to Use
+
+* Enter your bill Amount and Cash Given
+
+* You will get the change to return with no of notes to be handed out.
+
+* (BONUS) Cash given input is only avalable if the bill amount button is clicked 
+
+* (BONUS) Only notes that are to be returned is shown in table.
+
+## Tech Stack
+
+* HTML
+
+* CSS
+
+* Javascript
---
+++ index.js
@@ -0,0 +1,73 @@
+let billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const nextButton = document.querySelector("#next");
+const checkButton = document.querySelector("#check-button");
+const message = document.querySelector("#error-message");
+const hideCashGiven = document.querySelector(".hide-cash-given");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+const noteCol = document.querySelectorAll(".note-col");
+const displayTable = document.querySelector(".change-table");
+
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+
+billAmount.addEventListener("input", () => {
+  hideCashGiven.style.display = "none";
+  displayTable.style.display = "none";
+  cashGiven.value = "";
+});
+
+nextButton.addEventListener("click", function () {
+  if (billAmount.value === "") {
+    showMessage("Enter Bill Amount");
+  } else {
+    hideCashGiven.style.display = "flex";
+  }
+});
+
+checkButton.addEventListener(
+  "click",
+  function validateBillsAmountAndCashAmount() {
+    hideMessage();
+    let amount = Number(billAmount.value);
+    let cash = Number(cashGiven.value);
+
+    if (amount >= 0) {
+      if (cash > amount) {
+        const change = cash - amount;
+        showMessage(`The change to be returned is ${change}`);
+        calculateChange(change);
+        displayTable.style.display = "block";
+      } else if (amount === cash) {
+        showMessage("No change to return");
+      } else {
+        showMessage("Do you wanna wash plates?");
+      }
+    } else {
+      showMessage("the bill amount should be greater than 0");
+    }
+  }
+);
+
+function calculateChange(change) {
+  availableNotes.forEach((notes, i) => {
+    noOfNotes[i].innerText = "";
+    const numberOfNotes = Math.trunc(change / availableNotes[i]);
+    change %= availableNotes[i];
+    if (numberOfNotes >= 1) {
+      noteCol[i].style.display = "table-cell";
+      noOfNotes[i].style.display = "table-cell";
+      noOfNotes[i].innerText = numberOfNotes;
+    } else {
+      noteCol[i].style.display = "none";
+      noOfNotes[i].style.display = "none";
+    }
+  });
+}
+function hideMessage() {
+  message.style.display = "none";
+}
+
+function showMessage(msg) {
+  message.style.display = "block";
+  message.innerText = msg;
+}
